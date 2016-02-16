38bb7eb548e898c3ad7f810c4a03eeb80529e147
Klaus Froese <klausfroese@gmail.com>
2016-02-16 09:47:30 +0100

    neue Datei:             public_html/.htaccess 	geändert:               public_html/configuration.php 	neue Datei:             public_html/css/gh-fork-ribbon.css 	neue Datei:             public_html/css/gh-fork-ribbon.ie.css 	geändert:               public_html/index.php 	neue Datei:             public_html/index.php.v0.3 	neue Datei:             public_html/language/lang.de_DE.php 	neue Datei:             public_html/language/lang.en_GB.php 	neue Datei:             public_html/language/lang.it_IT.php 	neue Datei:             public_html/language/lang.ru_RU.php
    Some more files, and a small notice to fork me on github.com

--- public_html/configuration.php
+++ public_html/configuration.php
@@ -1,11 +1,21 @@
 <?php 
-/*
-  THIS IS THE CONFIG FILE FOR NEXT GENERATION DASHBOARD 
-
-*/
+/* THIS IS THE CONFIG FILE FOR THE NEXT GENERATION DASHBOARD 2.0 */
 
 
-/* UNUSED DEFINITION USES A HASH TAG */
+/* BOOTWATCH CSS STYLING SHEET URL, SELECT ONLY ONE URL */
+#define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/yeti/bootstrap.min.css');
+#define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css');
+#define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cyborg/bootstrap.min.css');
+#define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/sandstone/bootstrap.min.css');
+#define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css');
+# war ori define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css');
+define('CSS_BOOTSTRAP_URL','//maxcdn.bootstrapcdn.com/bootswatch/3.3.6/spacelab/bootstrap.min.css');
+
+/* GENERAL SETTINGS */
+define('FOOTERTEXT','&nbsp;&copy; 2016 <a href="http://xrfmaster.net/wiki/" target=_blank>XRFMaster Team</a>, visit our website for further information.');
+define('CALLSIGNDB','http://qrz.com/db');
+
+
 
 define('PAGETITLE','XRF...'); 
 define('SUBTITLE',' ...... Reflector'); 
@@ -13,22 +23,34 @@ define('SYSOPCALL','.....');
 define('SYSOPNAME','.....');
 define('INFOTEXT','<p>Free and open D-Star Reflector</p>');
 define('INFOURL','#');
-define('FOOTERTEXT','&nbsp;&copy; 2015 NerdStar Team, visit our website for further information.');
+define('FOOTERTEXT','&nbsp;&copy; 2016 <a href="http://xrfmaster.net/wiki/" target=_blank>XRFMaster Team</a>, visit our website for further information.');
 define('CALLSIGNDB','http://qrz.com/db');
 
+/* HEADER PICTURE URL */
+#define('HEADER_PIC_URL','http://www.t08.net/images/schloss_neuburg.jpg');
+
+/* SOME INFOS ABOUT THE TOPIC OF EACH MODULE */
+define('DESC_MODUL_A',' XRF-Verbund D-A-CH ');
+define('DESC_MODUL_B',' Brandmeister TG4850 ');
+define('DESC_MODUL_C',' Echolink 85851 + Bayernwolke FM ');
+define('DESC_MODUL_D',' D-Star Region Bayern-Süd ');
+define('DESC_MODUL_E',' Parrot');
+
+/* DEFINE META TAGS HERE */
+define('METATAG_AUTHOR','DL5RFK Klaus');
 
 /* TOP BUTTONS DEFINITION */ 
 define('TOPBUTTON1TEXT','..');
 define('TOPBUTTON1LINK','../');
 
 define('TOPBUTTON2TEXT','Wiki');
-define('TOPBUTTON2LINK','http://digitalfunk.dl5rfk.org/wiki/');
+define('TOPBUTTON2LINK','http://xrfmaster.net/wiki/');
 
 define('TOPBUTTON3TEXT','XRFMaster');
 define('TOPBUTTON3LINK','http://www.xrfmaster.net/');
 
-define('TOPBUTTON4TEXT','Hostfiles');
-define('TOPBUTTON4LINK','http://vk4tux.duckdns.org/OD/');
+#define('TOPBUTTON4TEXT','Hostfiles');
+#define('TOPBUTTON4LINK','http://vk4tux.duckdns.org/OD/');
 
 #define('TOPBUTTON5TEXT','');
 #define('TOPBUTTON5LINK','#');
