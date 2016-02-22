70a40b010562cf047ceb3df37b396dfbd789d462
Klaus Froese <klausfroese@gmail.com>
2016-02-22 23:05:57 +0100

    a view changes

--- dxrfd_start_cli.sh
+++ dxrfd_start_cli.sh
@@ -3,15 +3,13 @@
 # ----------------------------------------------------------------------
 #title       : 
 #description : This script will help you to controll your DXRFD Reflector
-#author		 : DL5RFK
-#date        : 2015-11-06
+#author	     : DL5RFK
+#date        : 2016-02-06
 #version     : 0.4    
-#usage		 : bash ./dxrfd_start.cli.sh 
+#usage	     : bash ./dxrfd_start.cli.sh 
 #notes       : 
 #licenze     : GNU GPL v3
 # ----------------------------------------------------------------------
-
-
 
 echo
 echo "+-----------------------------------------------------------------+"
@@ -20,8 +18,10 @@ echo "|       You can control your own Reflector in a easy way          |"
 echo "|                                                                 |"
 echo "| Type one of the following commands and watchout the response    |"
 echo "|                                                                 |"
-echo "| To link a remote Reflector with your own type lrf               |"
+echo "| To link a remote Reflector with your own                        |"
 echo "| lrf [OwnModul^RemoteReflector^RemoteModul] Example: AXRF232A    |"
+echo "| To unlink a remote Reflector                                    |"
+echo "| lrf [OwnModul^RemoteReflector^X]           Example: AXRF232X    |"
 echo "|                                                                 |"
 echo "| List of other Commands                                          |"
 echo "| pu   => Print users                                             |"
@@ -33,7 +33,7 @@ echo "| qson => QSO Details no                                          |"
 echo "| sh   => Shutdown reflector                                      |"
 echo "| mu   => Mute users                                              |"
 echo "| uu   => Unmute users                                            |"
-echo "| upd  => Update Database                                         |"
+echo "| upd  => Update Database (e.g. if hostlist xrfs.ts is changed)   |"
 echo "|                                                                 |"
 echo "| Type strg+c to exit the cli                                     |"
 echo "|                                                                 |"
